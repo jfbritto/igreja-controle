106043ae64b6781fc8e0f00eb8d8036e97d21144
jfbritto <jf.britto@hotmail.com>
2019-04-17 23:23:19 -0300

    serviços no site

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -196,6 +196,44 @@
         </div>
       </div>
 
+
+      <!-- Project One Row -->
+      <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+        <div class="col-lg-6">
+          <img class="img-fluid" src="img/cells.jpg" alt="">
+        </div>
+        <div class="col-lg-6">
+          <div class="bg-black text-center h-100 project">
+            <div class="d-flex h-100">
+              <div class="project-text w-100 my-auto text-center text-lg-left">
+                <h4 class="text-white">Células</h4>
+                <p class="mb-0 text-white-50 text-justify">Mantenha a organização das células, envie os estudos da semana e disponibilize as informações necessárias para o seu bom funcionamento!</p>
+                <hr class="d-none d-lg-block mb-0 ml-0">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- Project Four Rows -->
+      <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+        <div class="col-lg-6">
+          <img class="img-fluid" src="img/folders.jpg" alt="">
+        </div>
+        <div class="col-lg-6 order-lg-first">
+          <div class="bg-black text-center h-100 project">
+            <div class="d-flex h-100">
+              <div class="project-text w-100 my-auto text-center text-lg-right">
+                <h4 class="text-white">Arquivos</h4>
+                <p class="mb-0 text-white-50 text-justify">Aposente os documentos de papel e a poeira acumulada! com o igreja controle você mantém todos os seus arquivos protegidos e disponíveis a todo instante com nosso serviço de armazenamento em nuvem.</p>
+                <hr class="d-none d-lg-block mb-0 mr-0">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </section>
 
